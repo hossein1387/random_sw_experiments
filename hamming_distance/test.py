fcaf79f7c9a0963d4d09f45dcdd1528b8c76b5db
Hossein <m.h.askari.hemmat@gmail.com>
2017-01-26 01:29:55 -0500

    adding automatic test

--- hamming_distance/test.py
+++ hamming_distance/test.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python
 __INPUT_FILE__ = "array.txt"
-array = []
 def hamming(a, b):
-    return bin(a^b).count('1')
+        return bin(a^b).count('1')
 
 if __name__ == "__main__":
-	with open(__INPUT_FILE__) as f:
-		for line in f:
-			array.append(int(line))
+    expected_min_hamming_distance = 33 # Result can't be bigger than this    
+    ERROR = False
+    with open(__INPUT_FILE__) as f:
+        for line in f:          
+            pairs    = line.replace(' ','').split(',')
+            if(pairs[0] == "min_hamming_dist"):
+                actual_min_hamming_distance = int(pairs[1])
+            else:
+                a        = int(pairs[0])            
+                b        = int(pairs[1])            
+                actual   = int(pairs[2])            
+                expected = hamming(a, b)
+                if(expected != actual):             
+                    print "ERROR: Hamming distance of %0d and %0d is %0d not %0d"%(a, b, expected, actual)
+                    ERROR = True
+                if(expected<expected_min_hamming_distance):
+                    expected_min_hamming_distance = expected
 
+    if(actual_min_hamming_distance!=expected_min_hamming_distance):
+        ERROR = True
+        print "min humming distance is %0d not %0d"%(expected_min_hamming_distance, actual_min_hamming_distance)
 
-
+    if(ERROR):
+        print "============================="
+        print "Test fail!"
+        print "============================="
+    else:
+        print "============================="
+        print "Test pass!"
+        print "============================="
